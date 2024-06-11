092177642f97ad3eb552d324e28871e4ab71d2b5
tnkcoll <theodore.collins2023@outlook.com>
2024-06-11 08:29:10 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Assignment6._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 0, 5, 7, 0, 9 };
+
+            int count = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != 0)
+                {
+                    arr[count++] = arr[i];
+                }
+            }
+
+            while (count < arr.Length)
+            {
+                arr[count++] = 0;
+            }
+            
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+        }
+    }
+}
